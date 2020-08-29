b5e2f8296212d9b23412b034885036de300a1996
Aaron Di Silvestro <aaron.disilvestro@nike.com>
2020-08-28 17:49:46 -0700

    debug doctests in travis

--- test.readme.sh
+++ test.readme.sh
@@ -5,11 +5,13 @@ set -e
 SOURCE=$(awk 'f{ if (/DOCTEST/){printf "%s", buf; f=0; buf=""} else buf = buf $0 ORS}; /DOCTEST/{f=1}' README.md)
 echo "${SOURCE}" > doctest.pl
 
+swipl -s doctest.pl -g "handler(json([fullName='Nicholas']), context(headers(['TZ'('PST')]), _), Response), write(Response)." -t halt
 T1=$(
 swipl -s doctest.pl -g "handler(json([fullName='Nicholas']), context(headers(['TZ'('PST')]), _), Response), write(Response)." -t halt | \
     jq '.possibleNames' | \
     jq 'length'
 )
+swipl -s doctest.pl -g "handler(json([]), context(headers(['TZ'('PST')]), _), Response), write(Response)." -t halt
 T2=$(
 swipl -s doctest.pl -g "handler(json([]), context(headers(['TZ'('PST')]), _), Response), write(Response)." -t halt | \
     jq '.possibleNames' | \
@@ -20,7 +22,7 @@ swipl -s doctest.pl -g "handler(json([]), context(headers(['TZ'('PST')]), _), Re
 [ -f assert.sh ] || wget https://raw.github.com/lehmannro/assert.sh/v1.1/assert.sh -O assert.sh &>/dev/null
 . assert.sh
 
-assert "echo '${T1}'" 2
-assert "echo '${T2}'" 5
+assert "echo '${T1}'" 1
+assert "echo '${T2}'" 4
 
 assert_end "doc_test"
