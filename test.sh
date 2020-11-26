ad1cbc91d8a4d1501fcb1f1324993ea89d00c040
Aaron Di Silvestro <aaron@bkrn.org>
2020-11-26 13:32:54 -0800

    integration tests in aws - not localstack

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-docker rm -f prolamb-localstack &> /dev/null || true
 ##──── build archives for test lambdas ───────────────────────────────────────────────────
 echo "Build Prolamb Docker Image"
 docker build --tag prolamb/prolamb:latest -f build.Dockerfile .
@@ -17,89 +16,59 @@ do
 done
 cd ..
 
-##──── Build our slightly special verion of local stack ──────────────────────────────────
-# Localstack has a built in suffix check depending on the lambda run time
-# For provided it searches for .sh by default but we have a .pl so we edit that in
-# the source and pass it in
-cd extern/localstack
-echo "Build modified localstack image"
-docker build -f localstack.Dockerfile --tag prolamb/localstack:latest . &> /dev/null
-cd ../../..
-docker run -p 4574:4574 -v /var/run/docker.sock:/var/run/docker.sock \
-    --privileged --name prolamb-localstack \
-    prolamb/localstack >test/localstack.log &
-echo "Wait for local stack to come up"
-(tail -f -n0 test/localstack.log &) | grep -q 'Ready.'
-
 echo "Terraform Init"
-cd test/terraform
+cd terraform
 terraform init
 terraform apply -auto-approve
 cd ..
 
 echo "Running tests"
 
-strip_status() {
-    local OPEN=0
-    local CLOSE=0
-    local S=""
-    for (( i=0; i<${#1}; i++ )); do
-        C=${1:$i:1}
-        if [[ "${C}" == "{" ]]; then 
-            ((++OPEN))
-        elif [[ "${C}" == "}" ]]; then 
-            ((++CLOSE)) 
-        fi
-        if (( OPEN > 0 )); then 
-            if (( OPEN >= CLOSE )) && [[ "${C}" != "\n" ]]; then 
-                local S="${S}${C}"
-            fi
-            if (( OPEN == CLOSE )); then 
-                i=${#1}
-            fi
-        fi
-    done
-    echo "${S}"
+invoke_function() {
+    local RESULT=$(aws lambda invoke --function-name $1 --payload "$2" /dev/stdout)
+    echo $RESULT | awk -F}{ '{print $1"}"}' | jq -Sc
 }
 
-# invoke_function() {
-#     local RESULT=$(awslocal lambda invoke --function-name $1 --payload "$2" /dev/stdout)
-#     strip_status "${RESULT}"# 
-# }
+expected() {
+    echo $1 | jq -Sc
+}
 
-# Success
-# SIMPLE=$(invoke_function ProlambSimple '{}')
-# CONTEXT=$(invoke_function ProlambContext '{}' | grep -o 'LANG-en_US.UTF-8')
-# EVENT=$(invoke_function ProlambEvent '{ "fullName": "William" }')
+# Expect Success
+SIMPLE=$(invoke_function ProlambSimple '{}')
+CONTEXT=$(invoke_function ProlambContext '{}' | grep -o 'LANG-en_US.UTF-8')
+EVENT=$(invoke_function ProlambEvent '{ "fullName": "William" }')
 
-# Failure
-# ERROR=$(invoke_function ProlambError '{}')
-# FAIL=$(invoke_function ProlambFail '{}')
-# UNBOUND=$(invoke_function ProlambUnbound '{}')
-# FALSE=$(invoke_function ProlambFalse '{}')
-# JSON_ERROR=$(invoke_function ProlambJsonError '{}')
-# SIMPLE_JSON_ERROR=$(invoke_function ProlambSimpleJsonError '{}')
-# BAD_MODULE=$(invoke_function ProlambBadModule '{}')
-# BAD_CALLABLE=$(invoke_function ProlambBadCallable '{}')
+# Expect Failure
+ERROR=$(invoke_function ProlambError '{}')
+FAIL=$(invoke_function ProlambFail '{}')
+UNBOUND=$(invoke_function ProlambUnbound '{}')
+FALSE=$(invoke_function ProlambFalse '{}')
+JSON_ERROR=$(invoke_function ProlambJsonError '{}')
+SIMPLE_JSON_ERROR=$(invoke_function ProlambSimpleJsonError '{}')
+BAD_MODULE=$(invoke_function ProlambBadModule '{}')
+BAD_CALLABLE=$(invoke_function ProlambBadCallable '{}')
 
-# echo "Adding assert"
+echo "Adding assert"
 
-# [ -f assert.sh ] || wget https://raw.github.com/lehmannro/assert.sh/v1.1/assert.sh -O assert.sh &>/dev/null
-# . assert.sh
+[ -f assert.sh ] || wget https://raw.github.com/lehmannro/assert.sh/v1.1/assert.sh -O assert.sh &>/dev/null
+
+md5sum --status -c checksums
+
+. assert.sh
 
 # Test success
-# assert "echo '${SIMPLE}'" '{"fullName":"William"}'
-# assert "echo '${CONTEXT}'" 'LANG-en_US.UTF-8'
-# assert "echo '${EVENT}'" '{"nickName":"Bob"}'
+assert "echo '${SIMPLE}'" '{"fullName":"William"}'
+assert "echo '${CONTEXT}'" 'LANG-en_US.UTF-8'
+assert "echo '${EVENT}'" '{"nickName":"Bob"}'
 
-# Test failure
-# assert "echo '${FAIL}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
-# assert "echo '${FALSE}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
-# assert "echo '${UNBOUND}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
-# assert "echo '${ERROR}'" '{"errorMessage":"This space intentionally left blank","errorType":"HandlerException"}'
-# assert "echo '${JSON_ERROR}'" '{"errorMessage":"I am JSON","errorType":"JsonError"}'
-# assert "echo '${SIMPLE_JSON_ERROR}'" '{"errorMessage":"json([name=SomeError,message=Description of Error])","errorType":"HandlerException"}'
-# assert "echo '${BAD_MODULE}'" '{"errorMessage":"Could not find module named grain","errorType":"InvalidHandlerModule"}'
-# assert "echo '${BAD_CALLABLE}'" '{"errorMessage":"Could not find callable named hand","errorType":"InvalidHandlerCallable"}'
+#  Test failure
+assert "echo '${FAIL}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
+assert "echo '${FALSE}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
+assert "echo '${UNBOUND}'" '{"errorMessage":"Handler predicate failed to resolve","errorType":"HandlerFailure"}'
+assert "echo '${ERROR}'" '{"errorMessage":"This space intentionally left blank","errorType":"HandlerException"}'
+assert "echo '${JSON_ERROR}'" '{"errorMessage":"I am JSON","errorType":"JsonError"}'
+assert "echo '${SIMPLE_JSON_ERROR}'" '{"errorMessage":"json([name=SomeError,message=Description of Error])","errorType":"HandlerException"}'
+assert "echo '${BAD_MODULE}'" '{"errorMessage":"Could not find module named grain","errorType":"InvalidHandlerModule"}'
+assert "echo '${BAD_CALLABLE}'" '{"errorMessage":"Could not find callable named hand","errorType":"InvalidHandlerCallable"}'
 
-# assert_end "simple invocation"
+assert_end "simple invocation"
--- test/terraform/test.tf
+++ test/terraform/test.tf
@@ -1,132 +1,122 @@
-provider "aws" {
-  version                     = "=2.34.0"
-  access_key                  = "mock_access_key"
-  region                      = "us-east-1"
-  s3_force_path_style         = true
-  secret_key                  = "mock_secret_key"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://127.0.0.1:4567"
-    cloudformation = "http://127.0.0.1:4581"
-    cloudwatch     = "http://127.0.0.1:4582"
-    dynamodb       = "http://127.0.0.1:4569"
-    es             = "http://127.0.0.1:4578"
-    firehose       = "http://127.0.0.1:4573"
-    iam            = "http://127.0.0.1:4593"
-    kinesis        = "http://127.0.0.1:4568"
-    lambda         = "http://127.0.0.1:4574"
-    route53        = "http://127.0.0.1:4580"
-    redshift       = "http://127.0.0.1:4577"
-    s3             = "http://127.0.0.1:4572"
-    secretsmanager = "http://127.0.0.1:4584"
-    ses            = "http://127.0.0.1:4579"
-    sns            = "http://127.0.0.1:4575"
-    sqs            = "http://127.0.0.1:4576"
-    ssm            = "http://127.0.0.1:4583"
-    stepfunctions  = "http://127.0.0.1:4585"
-    sts            = "http://127.0.0.1:4592"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
+provider "aws" {
+  region = "us-west-2"
+}
 
 resource "aws_lambda_function" "simple_lambda" {
   filename      = "../src/simple/bundle.zip"
   function_name = "ProlambSimple"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/simple/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "json_error_lambda" {
   filename      = "../src/json_error/bundle.zip"
   function_name = "ProlambJsonError"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/json_error/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "simple_json_error_lambda" {
   filename      = "../src/simple_json_error/bundle.zip"
   function_name = "ProlambSimpleJsonError"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/simple_json_error/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "error_lambda" {
   filename      = "../src/error/bundle.zip"
   function_name = "ProlambError"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/error/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "bad_module_lambda" {
   filename      = "../src/simple/bundle.zip"
   function_name = "ProlambBadModule"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "grain.handler"
   source_code_hash = "${filebase64sha256("../src/simple/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "bad_callable_lambda" {
   filename      = "../src/simple/bundle.zip"
   function_name = "ProlambBadCallable"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.hand"
   source_code_hash = "${filebase64sha256("../src/simple/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "context_lambda" {
   filename      = "../src/context/bundle.zip"
   function_name = "ProlambContext"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/context/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "event_lambda" {
   filename      = "../src/event/bundle.zip"
   function_name = "ProlambEvent"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/event/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "fail_lambda" {
   filename      = "../src/fail/bundle.zip"
   function_name = "ProlambFail"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/fail/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "false_lambda" {
   filename      = "../src/false/bundle.zip"
   function_name = "ProlambFalse"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/false/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
 
 resource "aws_lambda_function" "unbound_lambda" {
   filename      = "../src/unbound/bundle.zip"
   function_name = "ProlambUnbound"
-  role          = "dontmatter"
+  role          = "arn:aws:iam::869128890907:role/iam_for_lambda"
   handler       = "main.handler"
   source_code_hash = "${filebase64sha256("../src/unbound/bundle.zip")}"
   runtime = "provided"
+  timeout = 15
 }
